af71bbeaed81105a3f50b20dba2411b72a2e5111
lgsouza24 <46302577+lgsouza24@users.noreply.github.com>
2022-12-08 16:31:16 -0300

    updatesettings2

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['djangopro-production.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','djangopro-production.up.railway.app']
 
 
 # Application definition
